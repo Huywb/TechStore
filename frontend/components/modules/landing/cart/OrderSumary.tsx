474e65bdb3e240930e5165ce9b091e2fdcbfda1b
huywb <pthhuy1306@gmail.com>
2026-05-19 19:57:12 +0700

    (Fix) - add OrderSumary component on cart layout

---
+++ frontend/components/modules/landing/cart/OrderSumary.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { useStore } from '@/app/store/cart.store'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+
+const OrderSumary = () => {
+
+    const {total,subTotal} = useStore()
+
+  return (
+    <div className='bg-white p-4 flex flex-col gap-2 w-full h-fit'>
+        <h1 className='text-xl font-bold'>Order Summary</h1>
+        <div className='flex flex-col gap-2 text-sm'>
+            <div className='flex justify-between text-gray-500'>
+                <span>SubTotal</span>
+                <span className='font-bold text-black'>${subTotal.toFixed(2)}</span>
+            </div>
+            <div className='flex justify-between text-gray-500'>
+                <span>Discount</span>
+                <span className='font-bold text-black'>${(subTotal-total).toFixed(2)}</span>
+            </div>
+        </div>
+        <span className='h-[0.5] bg-gray-300'></span>
+        <div className='flex justify-between font-bold'>
+            <span>Total</span>
+            <span>${total.toFixed(2)}</span>
+        </div>
+        <Link href={'/'} className='p-2 w-full bg-shop_dark_green hover:bg-shop_light_green hoverEffect text-center rounded-full text-white'>Proceed to Checkout</Link>
+    </div>
+  )
+}
+
+export default OrderSumary
